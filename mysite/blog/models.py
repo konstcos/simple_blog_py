75560aef3d26c6ea97741f21b4d04460f248e5cc
konst.cos <konst.cos@gmail.com>
2019-09-23 19:18:40 +0300

    add templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
+    objects = models.Manager()  # Менеджер по умолчанию.
+    published = PublishedManager()  # Наш новый менеджер.
+
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -19,6 +29,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+        # db_table = 'name_database'
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug]
+        )
