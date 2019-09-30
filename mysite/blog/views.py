4391f4db333db705ebc51ade260438e8071ade86
konst.cos <konst.cos@gmail.com>
2019-09-30 19:37:22 +0300

    finish 2 chapter

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,10 +4,18 @@ from django.views.generic import ListView
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)  # По 3 статьи на каждой странице.
     page = request.GET.get('page')
 
@@ -22,7 +30,7 @@ def post_list(request):
         # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -51,6 +59,16 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # Формирование списка похожих статей.
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post\
+        .published\
+        .filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts\
+        .annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
@@ -58,7 +76,8 @@ def post_detail(request, year, month, day, post):
             'post': post,
             'comments': comments,
             'new_comment': new_comment,
-            'comment_form': comment_form
+            'comment_form': comment_form,
+            'similar_posts': similar_posts
         }
     )
 
